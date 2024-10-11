a24d171c40d7fcbe872a367ef168a28c8a15a682
hrishikesh14m <hrishikesh.muley14@gmail.com>
2024-10-12 00:22:27 +0530

    workbench 7.1 completed

--- Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.1/Main.java
@@ -1,3 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.name = "Hrishikesh";
+        person.dateOfBirth ="05/10/1993";
+        person.nationality = "Indian";
+        person.passport = new String[]{person.name , person.nationality, person.dateOfBirth };
+        person.seatNumber = 12;
+
+        System.out.println(person.name);
+        System.out.println(person.dateOfBirth);
+        System.out.println(person.nationality);
+        System.out.println(Arrays.toString(person.passport));
+        System.out.println(person.seatNumber);
+       
+    }
     
 }
