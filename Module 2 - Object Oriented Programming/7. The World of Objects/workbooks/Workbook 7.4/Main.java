39eb03ead44e73d3d6fd27f201f635849f3eaa95
hrishikesh14m <hrishikesh.muley14@gmail.com>
2024-10-13 13:39:55 +0530

    workbook 7.4 resolved

--- Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.4/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.4/Main.java
@@ -5,6 +5,12 @@
         person.setSeatNumber(10);
         System.out.println("Name: " + person.getName() + "\n" + "Nationality: " + person.getNationality() + "\n" + "Date of Birth: " + person.getDateOfBirth() + "\n" + "Seat Number: " + person.getSeatNumber() + "\n");
 
+        Person twin = new Person(person);
+        twin.setName("Jad Slim");
+        twin.setSeatNumber(3);
+
+        System.out.println("Name: " + twin.getName() + "\n" + "Nationality: " + twin.getNationality() + "\n" + "Date of Birth: " + twin.getDateOfBirth() + "\n" + "Seat Number: " + twin.getSeatNumber() + "\n");
+
     }
   
   
